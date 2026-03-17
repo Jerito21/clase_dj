96f8f67230fbff773c4edac3adbe09847fe64252
Jerito21 <jgil@gmail.com>
2026-03-17 11:14:02 -0500

    se sube el cambioo

--- invetario_django/dcrm/website/views.py
+++ invetario_django/dcrm/website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     return render(request, "home.html")
