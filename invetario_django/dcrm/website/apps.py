d8e7f9bb7dfc7c7a98925898b59d4c0a94a14db8
Jerito21 <jgil5270@gmail.com>
2026-06-21 16:25:49 -0500

    Documentar configuracion de la aplicacion en apps.py

--- invetario_django/dcrm/website/apps.py
+++ invetario_django/dcrm/website/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class WebsiteConfig(AppConfig):
+    """
+    Configuración de la aplicación 'website'.
+    Establece el tipo de campo clave auto-incremental por defecto (BigAutoField)
+    y registra el nombre modular de la aplicación dentro del proyecto Django.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'website'
