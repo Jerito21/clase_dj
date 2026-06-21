caaf8e56b7bf8e54ffee4e55b8f538ef037dda4d
Jerito21 <jgil5270@gmail.com>
2026-06-21 16:26:30 -0500

    Personalizar titulos y branding del panel de administracion de Django en urls.py

--- invetario_django/dcrm/dcrm/urls.py
+++ invetario_django/dcrm/dcrm/urls.py
@@ -1,21 +1,15 @@
 """
-URL configuration for dcrm project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Configuración de URLs del proyecto principal DCRM.
+Define los puntos de entrada principales del sitio: el panel administrativo global de Django
+y las rutas modulares de la aplicación 'website'.
 """
 from django.contrib import admin
 from django.urls import path, include
+
+# Personalización del sitio de administración de Django
+admin.site.site_header = "Administración del Sistema de Inventario DCRM"
+admin.site.site_title = "DCRM Admin"
+admin.site.index_title = "Gestión de Módulos del Sistema"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
